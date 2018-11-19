d966f56b36020c5a7c220ef4486d964880054679
Roya Norouzi <royanorouzi@my.unt.edu>
2018-11-19 16:12:29 -0600

    MultiplierCombinations

--- armstrong.py
+++ armstrong.py
@@ -1,10 +1,34 @@
+import itertools
 class Armstrong:
-	def __init__(self, inputF, inputDf, outputF, outputDf, FloMin, FloMax):
-		self.inputF = inputF
-		self.inputDf = inputDf
-		self.outputF=outputF
-		self.outputDf=outputDf
-		self.FloMin=FloMin
-		self.FloMax=FloMax
-	def productOfMultiplexers(self):
-		return (self.outputDf/float(self.inputDf))
+    def __init__(self, inputF, inputDf, outputF, outputDf, FloMin, FloMax):
+        self.inputF = inputF
+        self.inputDf = inputDf
+        self.outputF=outputF
+        self.outputDf=outputDf
+        self.FloMin=FloMin
+        self.FloMax=FloMax
+    def productOfMultiplexers(self):
+        return (self.outputDf/float(self.inputDf))
+    def primeFactors (self):
+        n=self.productOfMultiplexers()
+        I={2:0, 3:0, 5:0, 7:0, 11:0}
+        for i in I.keys():
+            while n%i==0:
+                I[i]+=1
+                n=n/i
+        return (I)
+    def MultiplierCombinaitons(self):
+        I=self.primeFactors ()
+        Ilist=[i for i in I.keys() for j in range(I[i])]
+        stuff = Ilist
+        productList=[]
+        for L in range(0, len(stuff)+1):
+            for subset in itertools.combinations(stuff, L):
+                product=1
+                for i in subset:
+                    product=i*product
+                    productList.append(product)
+        return (sorted(set(productList)))
+    #def bestMatch(self):
+        
+        
